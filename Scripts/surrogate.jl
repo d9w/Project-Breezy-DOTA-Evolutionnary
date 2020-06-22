3753188d2219aa05142b8fa8268210c093c261a8
Dennis G Wilson <dennis@d9w.xyz>
2020-06-22 02:02:22 +0200

    Update surrogate.jl

--- Scripts/surrogate.jl
+++ Scripts/surrogate.jl
@@ -1,16 +1,17 @@
 using CSV
 using DataFrames
 using Cambrian
-using Flux
+using Flux.Data: DataLoader
+using Flux: @epochs
 using CUDAapi
 if has_cuda()
     import CuArrays
     CuArrays.allowscalar(false)
 end
 
-function get_data(log_file="all_episodes.csv")
+function get_data(;log_file="all_episodes.csv", batch=1024)
     dat = CSV.File(log_file, header=false) |> DataFrame!
-    X = Array{Float64}(dat[:, 1:143])
+    X = Array{Float64}(dat[1:size(dat, 1)-1, 1:143])
     y = Array{Float64}(dat[:, 144:147])
     dy = diff(y, dims=1)
     dy[dy .< 0] .= 0.0
@@ -18,30 +19,35 @@ function get_data(log_file="all_episodes.csv")
     r = sum(dy, dims=2)
     test = rand(size(X, 1)) .< 0.2
     train_data = DataLoader(X[.~test, :]', r[.~test, :]', batchsize=batch, shuffle=true)
-    test_data = DataLoader(xtest, ytest, batchsize=batch)
+    test_data = DataLoader(X[test, :]', r[test, :]', batchsize=batch)
     train_data, test_data
 end
 
 function loss_all(dataloader, model)
     l = 0f0
     for (x,y) in dataloader
-        l += logitcrossentropy(model(x), y)
+        l += Flux.mse(model(x), y)
     end
     l/length(dataloader)
 end
 
+function build_model()
+	Chain(Dense(143, 128, relu),
+		  Dense(128, 128, relu),
+		  Dense(128, 64, relu),
+		  Dense(64, 32, relu),
+		  Dense(32, 1, relu))
+end
+
 function train_model(;device=cpu, n_epochs=10)
-    m = Chain(Dense(size(X, 2), 128, relu),
-              Dense(128, 64, relu),
-              Dense(64, 32, relu),
-              Dense(32, 1, relu))
+	m = build_model()
     train_data, test_data = get_data()
     train_data = device.(train_data)
     test_data = device.(test_data)
-    m = device.(m)
+    m = device(m)
     loss(x, y) = Flux.mse(m(x), y)
 
-    evalcb = () -> @show(loss_all(train_data, m))
+    evalcb = () -> @show((loss_all(train_data, m), loss_all(test_data, m)))
     @epochs n_epochs Flux.train!(loss, params(m), train_data, ADAM(), cb=evalcb)
     m
 end
